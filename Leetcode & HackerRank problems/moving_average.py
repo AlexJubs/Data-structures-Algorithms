6b2eddac9e75877578ac4e38683f9be7f6b86035
Alex Jabbour <redpod22@gmail.com>
2019-12-18 23:08:36 -0800

    Successful people inspire and charm others.

---
+++ Leetcode & HackerRank problems/moving_average.py
@@ -0,0 +1,18 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = list()
+        self.size = size
+
+    def next(self, val: int) -> float:
+        self.queue.append(val)
+        if len(self.queue) > self.size: self.queue.pop(0)
+        return sum(self.queue)/len(self.queue)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
