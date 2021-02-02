b7208d6f7bdce219687e4cc6225ed8a66082621f
Alex Jabbour <redpod22@gmail.com>
2021-02-02 18:18:59 -0500

    Update range_sum_query.py

--- range_sum_query.py
+++ range_sum_query.py
@@ -1,13 +1,18 @@
-class NumArray:
+class NumMatrix:
 
-    def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.sums = dict()
-    
-    def sumRange(self, i: int, j: int) -> int:
-        if self.sums.get((i,j)) != None: return self.sums.get[i,j]
-        return sum(self.nums[i:j+1])
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# param_1 = obj.sumRange(i,j)
+    def __init__(self, matrix: List[List[int]]):
+        self.prefix_sum_matrix = list()
+        buffer = list()
+        for x in matrix:
+            buffer = [x[0]]
+            for k in range(1,len(x)):
+                buffer.append(x[k]+buffer[-1])
+            self.prefix_sum_matrix.append(buffer)
+        print(self.prefix_sum_matrix)
+            
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        res = 0
+        for k in range(row1, row2+1):            
+            res += self.prefix_sum_matrix[k][col2]
+            if col1 != 0: res -= self.prefix_sum_matrix[k][col1-1]
+        return res
